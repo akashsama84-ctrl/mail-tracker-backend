f1406a5aea337931ba0e595465f686e036b1bd07
akashsama84-ctrl <akashsama84@gmail.com>
2026-02-05 18:14:08 +0530

    s

--- server.js
+++ server.js
@@ -96,7 +96,7 @@ app.post('/api/send', async (req, res) => {
    return res.json({ success: true, messageId: info.messageId });
   } catch (error) {
     console.error('[ERROR] Failed to send or save email:', error);
-    res.status(500).json({ success: false, error: error.message });
+  return  res.status(500).json({ success: false, error: error.message });
   }
 });
 
